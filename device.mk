f25a3c031dfa9ff1261dba26bd93107b6eaaca5a
Atakan <ata.kaner03@gmail.com>
2025-03-25 16:36:41 +0300

    x1q: Switch to Samsung sensors
    Change-Id: Ic6980dd74289fc9ab14bfe1d0fa531b669b5d6e4

--- device.mk
+++ device.mk
@@ -40,10 +40,10 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.x1q
+    sensors.samsung
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
