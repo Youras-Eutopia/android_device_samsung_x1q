630f1506f717d08327705a3e89daac6d2a07a251
DY3413 <milosix3413@gmail.com>
2025-07-15 01:23:08 +0900

    x1q: Commonize AAPT config

--- device.mk
+++ device.mk
@@ -6,10 +6,6 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/samsung/sm8250-common/common.mk)
 
-# AAPT
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
